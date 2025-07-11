d42ef7f4bcee290cd5397671b8ca949387065557
xsallos <adrian.gawron@bitapps.fi>
2025-07-11 14:39:21 +0200

    Remove utils artifact

--- src/core/utils.py
+++
@@ -1,6 +0,0 @@
-def sanitize_isoformat(value: str) -> str:
-    if value.endswith("Z+00:00"):
-        return value.replace("Z+00:00", "+00:00")
-    elif value.endswith("Z"):
-        return value.replace("Z", "+00:00")
-    return value
